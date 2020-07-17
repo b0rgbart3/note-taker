e1a1ca5b1a17ec0363e88fe74e0f849742a17ed5
Bart Dority <b0rgBart3@gmail.com>
2020-07-17 09:25:56 -0700

    setting things up

---
+++ test/get_started.test.js
@@ -0,0 +1,63 @@
+// Here we will test our server -- and it's very first route
+
+// const App = require("../server");
+
+// describe("App", () => {
+//   describe("get started", () => {
+//     it ("should return the notes.html file for the /notes route", async () =>
+//     {
+      
+//       const res = await request(app)
+//       .get('/notes')
+
+//     expect(res.statusCode).toEqual(200)
+//     console.log("The test reveals: the url is: " + res.url);
+
+
+//     })
+//   })
+// })
+
+// const Algo = require("../algo");
+
+// describe("Algo", () => {
+//   describe("reverse", () => {
+//     it("should reverse a given string", () => {
+//       const str = "Hello World!";
+//       const reversed = "!dlroW olleH";
+
+//       const result = new Algo().reverse(str);
+
+//       expect(result).toEqual(reversed);
+//     });
+//   });
+
+//   describe("isPalindrome", () => {
+//     it("should return true if a string is a palindrome", () => {
+//       const str = "racecar";
+
+//       const result = new Algo().isPalindrome(str);
+
+//       expect(result).toEqual(true);
+//     });
+
+//     it("should return false if a string is not a palindrome", () => {
+//       const str = "neon";
+
+//       const result = new Algo().isPalindrome(str);
+
+//       expect(result).toEqual(false);
+//     });
+//   });
+
+//   describe("capitalize", () => {
+//     it("should take a string and return a new string with the first letter of each word capitalized", () => {
+//       const str = "capitalize every first word of the string.";
+//       const capitalized = "Capitalize Every First Word Of The String.";
+
+//       const result = new Algo().capitalize(str);
+
+//       expect(result).toEqual(capitalized);
+//     });
+//   });
+// });
